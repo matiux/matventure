e099887231647f02893d6b1582653bcd8b9304ab
Matteo <matteo@adriasonline.it>
2019-05-06 18:01:41 +0200

    Arrivato al tutorial 5

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+	"image"
 	"image/color"
 
 	"github.com/matiux/matventure/systems"
@@ -10,73 +12,136 @@ import (
 	"github.com/EngoEngine/engo/common"
 )
 
-/**
-Scenes are the backbone of engo: they contain pretty much all other things within the game.
-You should have multiple scenes and switch between them: e.g. one for the main menu, another for the loading screen,
-and yet another for the in-game experience. Specifically, a Scene contains one World, a collection
-of multiple Systems and a magnitude of Entitys
-*/
+const (
+	KeyboardScrollSpeed = 400
+	EdgeScrollSpeed     = KeyboardScrollSpeed
+	EdgeWidth           = 20
+	ZoomSpeed           = -0.125
+)
 
-type City struct {
+// Ogni Scene ha un solo World. E questo World è una raccolta di System ed Entity.
+type myScene struct{}
+
+type HUD struct {
 	ecs.BasicEntity
 	common.RenderComponent
 	common.SpaceComponent
 }
 
-type myScene struct{}
-
-// Type uniquely defines your game type
-func (*myScene) Type() string { return "myGame" }
-
-// Preload is called before loading any assets from the disk,
-// to allow you to register / queue them
-func (*myScene) Preload() {
-	engo.Files.Load("textures/city.png")
+type Tile struct {
+	ecs.BasicEntity
+	common.RenderComponent
+	common.SpaceComponent
 }
 
-// Setup is called before the main loop starts. It allows you
-// to add entities and systems to your Scene.
+// Type definisce in modo univoco il tuo tipo di gioco
+func (*myScene) Type() string { return "myGame" }
+
+// Preload viene chiamato prima di caricare qualsiasi risorsa dal disco,
+// per consentire di registrarli / accodarli
+func (*myScene) Preload() {
+
+	engo.Files.Load("textures/city.png", "tilemap/TrafficMap.tmx")
+}
+
+// Setup viene chiamato prima dell'avvio del ciclo principale. Ti permette di aggiungere Entity e System alla tua scena.
 func (*myScene) Setup(u engo.Updater) {
-
-	engo.Input.RegisterButton("AddCity", engo.KeyF1)
-
+	world, _ := u.(*ecs.World)
 	common.SetBackground(color.White)
 
-	world, _ := u.(*ecs.World)
+	// An instance of the RenderSystem is added to the World of this specific Scene.
 	world.AddSystem(&common.RenderSystem{})
+	world.AddSystem(&common.MouseSystem{})
+
+	kbs := common.NewKeyboardScroller(KeyboardScrollSpeed, engo.DefaultHorizontalAxis, engo.DefaultVerticalAxis)
+	world.AddSystem(kbs)
+	//world.AddSystem(&common.EdgeScroller{EdgeScrollSpeed, EdgeWidth})
+	world.AddSystem(&common.MouseZoomer{ZoomSpeed})
+
+	// An instance of the CityBuildingSystem is added to the World of this specific Scene.
 	world.AddSystem(&systems.CityBuildingSystem{})
 
-	//city := City{BasicEntity: ecs.NewBasic()}
-	//city.SpaceComponent = common.SpaceComponent{
-	//	Position: engo.Point{10, 10},
-	//	Width:    303,
-	//	Height:   641,
-	//}
-	//
-	//texture, err := common.LoadedSprite("textures/city.png")
-	//
-	//if err != nil {
-	//	log.Println("Unable to load texture: " + err.Error())
-	//}
-	//
-	//city.RenderComponent = common.RenderComponent{
-	//	Drawable: texture,
-	//	Scale:    engo.Point{1, 1},
-	//}
-	//
-	//for _, system := range world.Systems() {
-	//	switch sys := system.(type) {
-	//	case *common.RenderSystem:
-	//		sys.Add(&city.BasicEntity, &city.RenderComponent, &city.SpaceComponent)
-	//	}
-	//}
+	hud := HUD{BasicEntity: ecs.NewBasic()}
+	hud.SpaceComponent = common.SpaceComponent{
+		Position: engo.Point{0, engo.WindowHeight() - 40},
+		Width:    200,
+		Height:   40,
+	}
+
+	hudImage := image.NewUniform(color.RGBA{205, 205, 205, 255})
+	hudNRGBA := common.ImageToNRGBA(hudImage, 200, 40)
+	hudImageObj := common.NewImageObject(hudNRGBA)
+	hudTexture := common.NewTextureSingle(hudImageObj)
+
+	hud.RenderComponent = common.RenderComponent{
+		Drawable: hudTexture,
+		Scale:    engo.Point{1, 1},
+		Repeat:   common.Repeat,
+	}
+
+	hud.RenderComponent.SetShader(common.HUDShader)
+	hud.RenderComponent.SetZIndex(1)
+
+	// And finally add it to the world:
+	for _, system := range world.Systems() {
+		switch sys := system.(type) {
+		case *common.RenderSystem:
+			sys.Add(&hud.BasicEntity, &hud.RenderComponent, &hud.SpaceComponent)
+		}
+	}
+
+	resource, err := engo.Files.Resource("tilemap/TrafficMap.tmx")
+	if err != nil {
+		panic(err)
+	}
+	tmxResource := resource.(common.TMXResource)
+	levelData := tmxResource.Level
+
+	tiles := make([]*Tile, 0)
+
+	for _, tileLayer := range levelData.TileLayers {
+
+		fmt.Println(fmt.Sprintf("Carico la mappa: %s.", tileLayer.Name))
+
+		for _, tileElement := range tileLayer.Tiles {
+
+			fmt.Println(fmt.Sprintf("Tile in posizione X: %f - Y %f", tileElement.X, tileElement.Y))
+
+			if tileElement.Image != nil {
+				tile := &Tile{BasicEntity: ecs.NewBasic()}
+				tile.RenderComponent = common.RenderComponent{
+					Drawable: tileElement,
+					Scale:    engo.Point{1, 1},
+				}
+				tile.SpaceComponent = common.SpaceComponent{
+					Position: tileElement.Point,
+					Width:    0,
+					Height:   0,
+				}
+				tiles = append(tiles, tile)
+			}
+		}
+	}
+
+	// add the tiles to the RenderSystem
+	for _, system := range world.Systems() {
+		switch sys := system.(type) {
+		case *common.RenderSystem:
+			for _, v := range tiles {
+				sys.Add(&v.BasicEntity, &v.RenderComponent, &v.SpaceComponent)
+			}
+		}
+	}
+
+	common.CameraBounds = levelData.Bounds()
 }
 
 func main() {
 	opts := engo.RunOptions{
-		Title:  "Hello World",
-		Width:  1024,
-		Height: 768,
+		Title:          "Hello World",
+		Width:          800,
+		Height:         641,
+		StandardInputs: true,
 	}
 	engo.Run(opts, &myScene{})
 }
--- systems/city_building_systems.go
+++ systems/city_building_systems.go
@@ -2,13 +2,30 @@ package systems
 
 import (
 	"fmt"
-
-	"github.com/EngoEngine/engo"
+	"log"
 
 	"github.com/EngoEngine/ecs"
+	"github.com/EngoEngine/engo"
+	"github.com/EngoEngine/engo/common"
 )
 
-type CityBuildingSystem struct{}
+// Entity
+type City struct {
+	ecs.BasicEntity
+	common.RenderComponent // holds information about what to render (i.e. which texture)
+	common.SpaceComponent  // holds information about where it should be rendered.
+}
+
+type MouseTracker struct {
+	ecs.BasicEntity
+	common.MouseComponent
+}
+
+type CityBuildingSystem struct {
+	world *ecs.World
+
+	mouseTracker MouseTracker
+}
 
 // Remove is called whenever an Entity is removed from the World,
 // in order to remove it from this sytem as well
@@ -16,13 +33,57 @@ func (*CityBuildingSystem) Remove(ecs.BasicEntity) {}
 
 // Update is ran every frame, with `dt` being
 // the time in seconds since the last frame
-func (*CityBuildingSystem) Update(dt float32) {
-
+func (cb *CityBuildingSystem) Update(dt float32) {
 	if engo.Input.Button("AddCity").JustPressed() {
 		fmt.Println("The gamer pressed F1")
+
+		// Entity
+		city := City{BasicEntity: ecs.NewBasic()}
+
+		// Entity - SpaceComponent
+		city.SpaceComponent = common.SpaceComponent{
+			Position: engo.Point{cb.mouseTracker.MouseX, cb.mouseTracker.MouseY},
+			Width:    30,
+			Height:   64,
+		}
+
+		// Entity - RenderComponent
+		texture, err := common.LoadedSprite("textures/city.png")
+		if err != nil {
+			log.Println("Unable to load texture: " + err.Error())
+		}
+
+		city.RenderComponent = common.RenderComponent{
+			Drawable: texture,
+			Scale:    engo.Point{0.1, 0.1},
+		}
+
+		// Aggiungo l'Entity al System
+		for _, system := range cb.world.Systems() {
+			switch sys := system.(type) {
+			case *common.RenderSystem:
+				// We’re using the RenderSystem-specific Add method to add our City to that system.
+				sys.Add(&city.BasicEntity, &city.RenderComponent, &city.SpaceComponent)
+			}
+		}
 	}
 }
 
-func (*CityBuildingSystem) New(*ecs.World) {
+func (cb *CityBuildingSystem) New(w *ecs.World) {
+
+	cb.world = w
+
 	fmt.Println("CityBuildingSystem was added to the Scene")
+
+	cb.mouseTracker.BasicEntity = ecs.NewBasic()
+	cb.mouseTracker.MouseComponent = common.MouseComponent{Track: true}
+
+	engo.Input.RegisterButton("AddCity", engo.KeyF1)
+
+	for _, system := range w.Systems() {
+		switch sys := system.(type) {
+		case *common.MouseSystem:
+			sys.Add(&cb.mouseTracker.BasicEntity, &cb.mouseTracker.MouseComponent, nil, nil)
+		}
+	}
 }
